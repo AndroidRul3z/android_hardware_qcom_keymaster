142f79ab20b57f1927914a6b12c93185e35417c6
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-27 22:32:37 +0200

    Enable heap mask compat flag globally
    All enabled CAF platforms need this, the only exception
    is Nexus devices.

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter msm8960 msm8974 msm8084,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
-ifeq ($(TARGET_BOARD_PLATFORM),msm8084)
+ifneq ($(TARGET_USES_AOSP),true)
 keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
 endif
 
